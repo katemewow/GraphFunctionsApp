717e2bf7740ebcc5237660b80abd655c15ae8316
Filllllll <filimonova-sasha@inbox.ru>
2024-01-19 01:28:51 +0300

    Sasha: v1

---
+++ src/main/java/com/example/graphfunctionsapp/Params.java
@@ -0,0 +1,24 @@
+package com.example.graphfunctionsapp;
+
+import org.json.JSONObject;
+
+public enum Params {
+    FUNC_NAME,
+    T_EMIT,
+    T_STEP,
+    T_END,
+    T_0,
+    PARAM1,
+    PARAM2,
+    X,
+    Y,
+    Z;
+
+    public static Double getDoubleValueOfJson(Params param, JSONObject json) {
+        return Double.valueOf((String) (json.get(param.name())));
+    }
+
+    public static Float getFloatValueOfJson(Params param, JSONObject json) {
+        return Float.valueOf((String) (json.get(param.name())));
+    }
+}
--- src/main/java/com/example/graphfunctionsapp/Server.java
+++ src/main/java/com/example/graphfunctionsapp/Server.java
@@ -1,10 +1,14 @@
 package com.example.graphfunctionsapp;
 
+import org.json.JSONObject;
+
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import static com.example.graphfunctionsapp.Params.*;
 
 public class Server implements Runnable {
     private final int port;
@@ -45,12 +49,33 @@ public class Server implements Runnable {
                     // Чтение самого сообщения
                     String jsonMessage = in.readUTF();
                     System.out.println("Получено от клиента:" + jsonMessage);
+//
 
+//                    // Обработка JSON и выполнение вычислений
+//                    String resultJson = processJson(jsonMessage);
+//
+//                    // Отправка результата обратно клиенту
+//                    out.println(resultJson);
+
+                    // new version
                     // Обработка JSON и выполнение вычислений
-                    String resultJson = processJson(jsonMessage);
+                    JSONObject json = new JSONObject(jsonMessage);
+                    System.out.println("Получено от клиента JSON:" + json);
 
-                    // Отправка результата обратно клиенту
-                    out.println(resultJson);
+                    Double t0 = getDoubleValueOfJson(T_0, json);
+                    Double tend = getDoubleValueOfJson(T_END, json);
+                    Double tstep = getDoubleValueOfJson(T_STEP, json);
+                    Float param1 = getFloatValueOfJson(PARAM1, json);
+                    Float param2 = getFloatValueOfJson(PARAM2, json);
+                    Function3D function = new Function3D();
+
+                    for (double x = t0; x <= tend; x += tstep) {
+                        for (double y = t0; y <= tend; y += tstep) {
+                            float z = function.compute((float)x, (float)y, param1, param2);
+                            out.println(createJsonToAnswer(x, y, z));
+                        }
+                    }
+                    out.println("end");
                 }
 
             } catch (IOException e) {
@@ -60,6 +85,16 @@ public class Server implements Runnable {
 
         private String processJson(String jsonMessage) {
             return jsonMessage;
+        }
+
+        private String createJsonToAnswer(Double x, Double y, Float z) {
+            // Сериализация данных в JSON
+            JSONObject json = new JSONObject();
+            json.put(X.name(), x);
+            json.put(Y.name(), y);
+            json.put(Z.name(), z);
+
+            return json.toString();
         }
     }
 }
